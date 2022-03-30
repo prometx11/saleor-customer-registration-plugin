3ed7a318dc5533878c1a94746ba2f5414b209db1
prometx <dj@arties.org>
2022-03-30 17:39:29 +0200

    added plugin id and description

--- custom_customer_registration/plugin.py
+++ custom_customer_registration/plugin.py
@@ -1,6 +1,9 @@
 from saleor.plugins.base_plugin import BasePlugin, ConfigurationTypeField
 
 class CustomCustomerRegistration(BasePlugin):
+    PLUGIN_ID = "plugin.customCustomerRegistration"  # plugin identifier
+    PLUGIN_NAME = "Custom Customer Registration"  # display name of plugin
+    PLUGIN_DESCRIPTION = "Enables customer registration with address."
     
     def customer_updated(self, customer: "User"):
         print("CustomCustomerRegistration customer_updated hook invoked...",customer)
