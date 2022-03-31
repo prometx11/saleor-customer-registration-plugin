f0ca0fce2990347cc64560e87f1ed4c9756e3fe8
prometx <dj@arties.org>
2022-03-31 04:29:01 +0200

    testing metadata & addresses

--- custom_customer_registration/plugin.py
+++ custom_customer_registration/plugin.py
@@ -1,5 +1,6 @@
 from typing import Any
 from saleor.plugins.base_plugin import BasePlugin, ConfigurationTypeField
+from saleor.account.models import Address
 
 class CustomCustomerRegistration(BasePlugin):
     PLUGIN_ID = "plugin.customCustomerRegistration"  # plugin identifier
@@ -7,8 +8,13 @@ class CustomCustomerRegistration(BasePlugin):
     PLUGIN_DESCRIPTION = "Enables customer registration with address."
     
     def customer_updated(self, customer: "User", previous_value: Any) -> Any:
-        print("CustomCustomerRegistration customer_updated hook invoked...",customer)
-        customer.first_name = "test132"
-        update_fields = ["first_name"]
-        customer.save(update_fields=update_fields)
+        print("CustomCustomerRegistration customer_updated hook invoked...",customer.metadata)
+        # customer.first_name = "test132"
+        # update_fields = ["first_name"]
+        # customer.save(update_fields=update_fields)
+        
+        addr = Address()
+        addr.first_name = "test123"
+        addr.user_addresses.add(customer)
+        
         
