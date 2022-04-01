e5818470e40ab382226b128a0bd85d37ca62b320
prometx <dj@arties.org>
2022-04-01 03:31:03 +0200

    wip

--- custom_customer_registration/plugin.py
+++ custom_customer_registration/plugin.py
@@ -12,15 +12,19 @@ class CustomCustomerRegistration(BasePlugin):
         # customer.first_name = "test132"
         # update_fields = ["first_name"]
         # customer.save(update_fields=update_fields)
-        
-        addr = Address()
-        addr.first_name = "firstname"
-        addr.last_name= "lastname"
-        addr.street_address_1 = "test street"
-        addr.city = "test city"
-        addr.country = "AT"
-        addr.save()
-        
-        addr.user_addresses.add(customer)
-        customer.addresses.add(addr)
+        if(customer.is_active): 
+            print("customer activate")
+            if("billing_address" in customer.metadata):
+                print("customer has billing address metadata")                      
+                addr = Address()
+                addr.first_name = "firstname"
+                addr.last_name= "lastname"
+                addr.street_address_1 = "test street"
+                addr.city = "test city"
+                addr.postal_code = "1223"
+                addr.country = "AT"
+                addr.save()
+                
+                addr.user_addresses.add(customer)
+                customer.addresses.add(addr)
         
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='custom-customer-registration',
-    version='0.1.2',
+    version='0.1.3',
     description='Saleor plugin for customer registration with address.',    
     # long_description='Saleor plugin for customer registration with address.',
     # url='https://github.com/',
